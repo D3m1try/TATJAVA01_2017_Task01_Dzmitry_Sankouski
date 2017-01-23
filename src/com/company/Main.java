1e98b219c88b29b7c615992c558efcff239d0620
Dzmitry Sankouski <Dzmitry_Sankouski@epam.com>
2017-01-23 15:31:50 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        final String USAGE = "1 param. - Integer";
+        int number = 0;
+        if (args.length != 1){
+            System.out.println(USAGE);
+            return;
+        }
+
+        try {
+            number = Integer.valueOf(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println(USAGE);
+            return;
+        }
+
+        System.out.println(((number%10000)/1000+(number%1000)/100)==((number%100)/10+(number%10)/1));
+    }
+}
